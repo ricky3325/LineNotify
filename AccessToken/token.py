52bb1c3214843a9e918ab909c95a82d8f393789a
ricky3325 <rick3325@instance-1.asia-east1-c.c.linenotify-331014.internal>
2021-11-08 09:41:09 +0000

    change to ORG

---
+++ AccessToken/token.py
@@ -0,0 +1,11 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+
+  url = "https://notify-bot.line.me/oauth/token"
+  headers = CaseInsensitiveDict()
+  headers["Accept"] = "application/json"
+  codeInput = '000'
+  print("Your Code: "+codeInput)
+  payload = dict(grant_type='authorization_code', code=codeInput, redirect_uri='http://35.194.229.108:5000/getCode', client_id='OdvneRVOej87cv2iir9tma', client_secret='EPBYbso4W0ygDMP5ivTJWxsjsUqISAXL34UNh6BXT5B')
+  resp = requests.post(url, data=payload)
+  print(resp.text)
--- LineService/app.py
+++ LineService/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for, request
-from .. import  AccessToken.app as 
+from flask import Flask, render_template, url_for, request 
 app = Flask(__name__)
 
 @app.route('/')
