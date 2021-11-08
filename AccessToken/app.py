4d0240d1c90c3fc3a7a0771828b546e26c06fb10
Coconut0817 <93178927+Coconut0817@users.noreply.github.com>
2021-11-08 15:34:58 +0800

    Update app.py

--- AccessToken/app.py
+++ AccessToken/app.py
@@ -1,15 +1,12 @@
 import requests
 from requests.structures import CaseInsensitiveDict
-
-url = "https://notify-bot.line.me/oauth/token"
-
-headers = CaseInsensitiveDict()
-headers["Accept"] = "application/json"
-
-codeInput='b9vuLWBbxIWW4eAdEmmElU'
-print("Your Code: "+codeInput)
-
-payload = dict(grant_type='authorization_code', code=codeInput, redirect_uri='http://35.194.229.108:5000/getCode', client_id='OdvneRVOej87cv2iir9tma', client_secret='EPBYbso4W0ygDMP5ivTJWxsjsUqISAXL34UNh6BXT5B')
-
-resp = requests.post(url, data=payload)
-print(resp.text)
+class Connect:
+  def __init__(self, code):
+    self.url = "https://notify-bot.line.me/oauth/token"
+    self.headers = CaseInsensitiveDict()
+    self.headers["Accept"] = "application/json"
+    self.codeInput = code
+    print("Your Code: "+codeInput)
+    payload = dict(grant_type='authorization_code', code=codeInput, redirect_uri='http://35.194.229.108:5000/getCode', client_id='OdvneRVOej87cv2iir9tma', client_secret='EPBYbso4W0ygDMP5ivTJWxsjsUqISAXL34UNh6BXT5B')
+    resp = requests.post(url, data=payload)
+    print(resp.text)
