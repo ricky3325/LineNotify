c0b3a6b7df67c6604c03161f9220493db552eeb4
ricky3325 <rick3325@instance-1.asia-east1-c.c.linenotify-331014.internal>
2021-11-08 07:13:59 +0000

    add LineService

---
+++ LineService/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello！'
+
+@app.route('/getCode', methods=['POST'])
+def Do_editInfo():
+    code = request.form["code"]
+    state = request.form["state"]
+    return code
+
+app.run(host='0.0.0.0')
