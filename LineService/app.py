8600e1b07bd712cd2392da3f2465482f188d4ea4
ricky3325 <rick3325@instance-1.asia-east1-c.c.linenotify-331014.internal>
2021-11-09 08:45:29 +0000

    add MQ publisher

--- LineService/app.py
+++ LineService/app.py
@@ -1,3 +1,4 @@
+import pika
 from flask import Flask, render_template, url_for, request 
 app = Flask(__name__)
 
@@ -9,6 +10,22 @@ def index():
 def Do_editInfo():
     code = request.form["code"]
     state = request.form["state"]
-    return code
+    
+    # push code
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', port=5672))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='task_queue', durable=True)
+    
+    channel.basic_publish(
+        exchange='',
+        routing_key='task_queue',
+        body=code,
+        properties=pika.BasicProperties(delivery_mode=2)
+    )
+
+    connection.close()
+
+    return "Success"
 
 app.run(host='0.0.0.0')
